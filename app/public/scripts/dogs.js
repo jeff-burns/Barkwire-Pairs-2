16049e314107e12a82e34362f22dc40c470d3fbf
Landon Folkers <32934735+landonfolkers@users.noreply.github.com>
2018-05-02 20:32:15 -0600

    fixed semicolons

--- app/public/scripts/dogs.js
+++ app/public/scripts/dogs.js
@@ -1,5 +1,5 @@
-const id = window.location.pathname.split('/')[2]
-const url =`http://localhost:3000/api/dogs/${id}`
+const id = window.location.pathname.split('/')[2];
+const url =`http://localhost:3000/api/dogs/${id}`;
 
 const getData = () => {
     return fetch(url)
@@ -8,9 +8,9 @@ const getData = () => {
             document.querySelectorAll(".dog")[0].innerHTML = `
                 <h3 class="name"><a href="/dogs/${res.dog.id}">${res.dog.name}</a></h3>
                 <img class="profile-picture" src="/${res.dog.profilePicture}" alt="${res.dog.name}"/>
-                <p class="bio">${res.dog.bio}</p>`
+                <p class="bio">${res.dog.bio}</p>`;
         })
-        .catch(err => console.error(err))
+        .catch(err => console.error(err));
 }
 
-getData()
+getData();
